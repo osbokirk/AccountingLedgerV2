fea0c9a85e58e6a4f394bc7dd9c15ac8a8984b3b
osbokirk <Kirk.c.osborne@gmail.com>
2024-07-02 13:07:06 -0500

    intial files added

---
+++ AccountingLedgerVersion2/src/main/java/com/example/AccountingLedgerVersion2/AccountingLedgerVersion2Application.java
@@ -0,0 +1,13 @@
+package com.example.AccountingLedgerVersion2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingLedgerVersion2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountingLedgerVersion2Application.class, args);
+	}
+
+}
