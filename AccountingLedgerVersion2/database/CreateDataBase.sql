7d7dbfb0bdcd112590ea0bb6ac1e69522f2adaf8
osbokirk <Kirk.c.osborne@gmail.com>
2024-07-03 13:38:17 -0500

    Database Creation Script Added

---
+++ AccountingLedgerVersion2/database/CreateDataBase.sql
@@ -0,0 +1,25 @@
+USE sys;
+
+# ---------------------------------------------------------------------- #
+# Target DBMS:           MySQL                                           #
+# Project name:          accountingLedger                                      #
+# ---------------------------------------------------------------------- #
+DROP DATABASE IF EXISTS accountingLedger;
+
+CREATE DATABASE IF NOT EXISTS accountingLedger;
+
+USE accountingLedger;
+
+# ---------------------------------------------------------------------- #
+# Tables                                                                 #
+# ---------------------------------------------------------------------- #
+
+CREATE TABLE users (
+    transaction_id INT NOT NULL AUTO_INCREMENT,
+    date VARCHAR(50) NOT NULL,
+    time VARCHAR(50) NOT NULL,
+    description VARCHAR(50) NOT NULL,
+    vendor VARCHAR(255) NOT NULL,
+    amount float NOT NULL,
+    PRIMARY KEY (transaction_id)
+);
