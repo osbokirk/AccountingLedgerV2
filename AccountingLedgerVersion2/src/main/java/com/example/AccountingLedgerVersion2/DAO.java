37d1b7a410db19baf9f77b0ef389def8b0bd6ad8
ahmawad <ahmawad.aa@gmail.com>
2024-07-18 15:21:12 -0500

    tweaked DAO

--- AccountingLedgerVersion2/src/main/java/com/example/AccountingLedgerVersion2/DAO.java
+++ AccountingLedgerVersion2/src/main/java/com/example/AccountingLedgerVersion2/DAO.java
@@ -4,104 +4,124 @@ import java.sql.*;
 
 
 public class DAO {
-    public ResultSet showAllLedgers(int userId) throws SQLException {
-        String query = "{CALL ShowAllLedgers(?)}";
+    public ResultSet showAllLedgers() throws SQLException {
+
+        String query = "{CALL ShowAllLedgers}";
+
         Connection conn = DatabaseConnection.getConnection();
         CallableStatement statement = conn.prepareCall(query);
-        statement.setInt(1, userId);
+
         return statement.executeQuery();
     }
 
-    public ResultSet showAllDeposits(int userId) throws SQLException {
-        String query = "{CALL ShowAllDeposits(?)}";
+    public ResultSet showAllDeposits() throws SQLException {
+
+        String query = "{CALL ShowAllDeposits}";
+
         Connection conn = DatabaseConnection.getConnection();
         CallableStatement statement = conn.prepareCall(query);
-        statement.setInt(1, userId);
+
         return statement.executeQuery();
     }
 
-    public ResultSet showAllPayments(int userId) throws SQLException {
-        String query = "{CALL ShowAllPayments(?)}";
+    public ResultSet showAllPayments() throws SQLException {
+
+        String query = "{CALL ShowAllPayments}";
+
         Connection conn = DatabaseConnection.getConnection();
         CallableStatement statement = conn.prepareCall(query);
-        statement.setInt(1, userId);
+
         return statement.executeQuery();
     }
-    public ResultSet showReportsByMonthToDate(int userId) throws SQLException {
+    public ResultSet showReportsByMonthToDate(Date startDate) throws SQLException {
+
         String query = "{CALL ShowReportsByMonthToDate(?)}";
+
         Connection conn = DatabaseConnection.getConnection();
         CallableStatement statement = conn.prepareCall(query);
-        statement.setInt(1, userId);
+        statement.setDate(1, startDate);
         return statement.executeQuery();
     }
 
-    public ResultSet showReportsByPreviousMonth(int userId) throws SQLException {
-        String query = "{CALL ShowReportsByPreviousMonth(?)}";
+    public ResultSet showReportsByPreviousMonth(Date startDate, Date endDate) throws SQLException {
+
+        String query = "{CALL ShowReportsByPreviousMonth(?,?)}";
+
         Connection conn = DatabaseConnection.getConnection();
         CallableStatement statement = conn.prepareCall(query);
-        statement.setInt(1, userId);
+        statement.setDate(1, startDate);
+        statement.setDate(2, endDate);
         return statement.executeQuery();
     }
 
-    public ResultSet showReportsByYearToDate(int userId) throws SQLException {
+    public ResultSet showReportsByYearToDate(Date startDate) throws SQLException {
+
         String query = "{CALL ShowReportsByYearToDate(?)}";
+
         Connection conn = DatabaseConnection.getConnection();
         CallableStatement statement = conn.prepareCall(query);
-        statement.setInt(1, userId);
+        statement.setDate(1, startDate);
         return statement.executeQuery();
     }
 
-    public ResultSet showReportsByPreviousYear(int userId) throws SQLException {
-        String query = "{CALL ShowReportsByPreviousYear(?)}";
+    public ResultSet showReportsByPreviousYear(Date startDate, Date endDate) throws SQLException {
+
+        String query = "{CALL ShowReportsByPreviousYear(?,?)}";
+
         Connection conn = DatabaseConnection.getConnection();
         CallableStatement statement = conn.prepareCall(query);
-        statement.setInt(1, userId);
+        statement.setDate(1, startDate);
+        statement.setDate(2, endDate);
         return statement.executeQuery();
     }
 
-    public ResultSet searchByVendor(int userId, String vendor) throws SQLException {
-        String query = "{CALL SearchByVendor(?, ?)}";
+    public ResultSet searchByVendor(String vendor) throws SQLException {
+
+        String query = "{CALL SearchByVendor(?)}";
+
         Connection conn = DatabaseConnection.getConnection();
         CallableStatement statement = conn.prepareCall(query);
-        statement.setInt(1, userId);
-        statement.setString(2, vendor);
+
+        statement.setString(1, vendor);
         return statement.executeQuery();
     }
 
-    public ResultSet customSearch(int userId, Date startDate, Date endDate, String vendor) throws SQLException {
-        String query = "{CALL CustomSearch(?, ?, ?, ?)}";
+    public ResultSet customSearch(Date startDate, Date endDate, String vendor) throws SQLException {
+
+        String query = "{CALL CustomSearch(?, ?, ?)}";
+
         Connection conn = DatabaseConnection.getConnection();
         CallableStatement statement = conn.prepareCall(query);
-        statement.setInt(1, userId);
-        statement.setDate(2, startDate);
-        statement.setDate(3, endDate);
-        statement.setString(4, vendor.isEmpty() ? "%" : vendor);
+
+        statement.setDate(1, startDate);
+        statement.setDate(2, endDate);
+        statement.setString(3, vendor.isEmpty() ? "%" : vendor);
         return statement.executeQuery();
     }
-    public void addDeposit(int userId, String description, String vendor, double amount) throws SQLException {
-        String query = "{CALL AddDeposit(?, ?, ?, ?)}";
+    public void addDeposit(String description, String vendor, double amount) throws SQLException {
+
+        String query = "{CALL AddDeposit(?, ?, ?)}";
 
         try (Connection conn = DatabaseConnection.getConnection();
              CallableStatement statement = conn.prepareCall(query)) {
 
-            statement.setInt(1, userId);
-            statement.setString(2, description);
-            statement.setString(3, vendor);
-            statement.setDouble(4, amount);
+            statement.setString(1, description);
+            statement.setString(2, vendor);
+            statement.setDouble(3, amount);
             statement.executeUpdate();
         }
     }
 
-    public void makePayment(int userId, String description, String vendor, double amount) throws SQLException {
-        String query = "{CALL MakePayment(?, ?, ?, ?)}";
+    public void makePayment(String description, String vendor, double amount) throws SQLException {
+
+        String query = "{CALL MakePayment(?, ?, ?)}";
 
         try (Connection conn = DatabaseConnection.getConnection();
              CallableStatement statement = conn.prepareCall(query)) {
 
-            statement.setInt(1, userId);
-            statement.setString(2, description);
-            statement.setString(3, vendor);
-            statement.setDouble(4, amount);
+            statement.setString(1, description);
+            statement.setString(2, vendor);
+            statement.setDouble(3, amount);
             statement.executeUpdate();
         }
     }
